d91c99086754fd12c7ef30905963f32d47f4f673
tbx200 <tristan.bakx@hotmail.com>
2018-03-28 15:02:09 +0200

    updated rcode

--- RBS_forsmar_cc.R
+++ RBS_forsmar_cc.R
@@ -1,5 +1,10 @@
 
 install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("tibble")
+install.packages("lubridate")
+library(lubridate)
+library(tibble)
 library(ggplot2)
 library(readr)
 
@@ -28,24 +33,36 @@ forsmark_clearcuts <- read_csv("/Volumes/TRISTANMAC/AgriBirds Sweden/QGIS and da
 ## Data preparation ##
 ######################
 
+forsmar <- as_tibble(forsmar)
+forsmark_clearcuts <- as_tibble(forsmark_clearcuts)
+
 ## Calculate numerical dates
+forsmark_clearcuts$Avvdatum <- as.POSIXct(forsmark_clearcuts$Avvdatum)
+forsmar$Date <- as.POSIXct(forsmar$Date)
+forsmar$Avvdatum <- as.POSIXct(forsmar$Avvdatum)
 forsmar$birdday <- as.numeric(forsmar$Date)
 forsmar$clearcutday <- as.numeric(forsmar$Avvdatum)
+forsmar$month <- month(forsmar$Date)
 
 ## Select complete cases
 forsmarsub <- forsmar[complete.cases(forsmar),]
+forsmarsub <- forsmarsub[forsmarsub$month<7,]
+forsmarsub$Inds <- as.numeric(forsmarsub$Inds)
 forsmarsub2 <- forsmarsub[,c(6,9,10,11,12,13,14,15,20)]
 forsmarsub2$day <- as.numeric(forsmarsub2$Avvdatum)
 
 forsmark_clearcuts$day <- as.numeric(forsmark_clearcuts$Avvdatum)
 
-forsmark_clearcuts <- data.frame(forsmark_clearcuts)
-forsmarsub2 <- data.frame(forsmarsub2)
 
 forsmark_clearcuts$type <- "clearcuts"
 forsmarsub2$type <- "birds"
 
 all <- rbind(forsmark_clearcuts,forsmarsub2)
+
+forsmarsub$Date <- as.Date(forsmarsub$Date)
+forsmarsub$Avvdatum <- as.Date(forsmarsub$Avvdatum)
+
+faulty_points <- forsmarsub[forsmarsub$Date<forsmarsub$Avvdatum,c(1:6,15)]
 
 ###########
 ## Plots ##
@@ -57,10 +74,15 @@ theme_d <- theme(axis.text = element_text(size = 12, color = "black"),
                  axis.line = element_line(color = "black"),
                  legend.text = element_text(size = 14, color = "black"))
 
-## Plot of cleacutting day and bird survey date
-ggplot(forsmarsub, aes(clearcutday, birdday)) +
+## Plot of clearcutting day and bird survey date
+lims <- c(as.Date("2002-08-01", "%Y-%m-%d"),as.Date("2017-08-01", "%Y-%m-%d"))
+
+ggplot(forsmarsub, aes(Avvdatum, Date)) +
   geom_point() +
-  geom_smooth() +
+  geom_smooth(method = "lm") +
+  geom_abline(slope = 1) +
+  scale_x_date(limits = lims) +
+  scale_y_date(limits = lims) +
   theme_classic() +
   labs(x = "Date of clearcutting", y = "Date of RBS sighting") +
   theme_d
@@ -122,6 +144,9 @@ sd(forsmark_clearcuts$day)
 mean(forsmarsub2$Avvdatum)
 median(forsmarsub2$day)
 sd(forsmarsub2$day)
+
+t.test(forsmark_clearcuts$day,forsmarsub2$day, var.equal=FALSE, paired=FALSE)
+
 
 
 
