6d4306bb256ba219b3dc568eba69500572a27d76
tbx200 <tristan.bakx@hotmail.com>
2018-04-03 16:08:03 +0200

    new histogram for age and start with mixed models

--- RBS_forsmar_cc.R
+++ RBS_forsmar_cc.R
@@ -1,12 +1,17 @@
+##############
+## Packages ##
+##############
 
 install.packages("ggplot2")
 install.packages("tidyverse")
 install.packages("tibble")
 install.packages("lubridate")
+install.packages("lme4")
 library(lubridate)
 library(tibble)
 library(ggplot2)
 library(readr)
+library(lme4)
 
 ##################
 ## Loading data ##
@@ -44,35 +49,56 @@ forsmar$birdday <- as.numeric(forsmar$Date)
 forsmar$clearcutday <- as.numeric(forsmar$Avvdatum)
 forsmar$month <- month(forsmar$Date)
 
-## Select complete cases
+## Select only the records for bird observations
 forsmarsub <- forsmar[complete.cases(forsmar),]
+
+## Exclude summer surveys
 forsmarsub <- forsmarsub[forsmarsub$month<7,]
+
+## Make number of individuals numeric
 forsmarsub$Inds <- as.numeric(forsmarsub$Inds)
+
+## Select points where clear cut was cut before date of bird observation
+forsmarsub <- forsmarsub[forsmarsub$Date>forsmarsub$Avvdatum,]
+
+## Calculate clearcut age in difference in days
+forsmarsub$clearcutage <- year(forsmarsub$Date) - year(forsmarsub$Avvdatum)
+
+## Subset nessecary columns from bird observation data
 forsmarsub2 <- forsmarsub[,c(6,9,10,11,12,13,14,15,20)]
+
+## Extract numerical date of clearcutting
+# For bird observation clear cut
 forsmarsub2$day <- as.numeric(forsmarsub2$Avvdatum)
 
+# For all clear cuts
 forsmark_clearcuts$day <- as.numeric(forsmark_clearcuts$Avvdatum)
 
-
+# Tag datasets with their origin
 forsmark_clearcuts$type <- "clearcuts"
 forsmarsub2$type <- "birds"
 
+# Combine data from all clearcuts, with the data from the bird 
+# observations
 all <- rbind(forsmark_clearcuts,forsmarsub2)
 
+## format dates as date
 forsmarsub$Date <- as.Date(forsmarsub$Date)
 forsmarsub$Avvdatum <- as.Date(forsmarsub$Avvdatum)
-
-faulty_points <- forsmarsub[forsmarsub$Date<forsmarsub$Avvdatum,c(1:6,15)]
 
 ###########
 ## Plots ##
 ###########
 
-#theme
+# theme for ggplot
 theme_d <- theme(axis.text = element_text(size = 12, color = "black"),
                  axis.title = element_text(size = 14, color = "black"),
                  axis.line = element_line(color = "black"),
                  legend.text = element_text(size = 14, color = "black"))
+hist_theme <- theme(axis.text = element_text(size = 12, color = "black"),
+                    axis.title = element_text(size = 14, color = "black"),
+                    axis.line = element_line(color = "black"),
+                    legend.position = "none") 
 
 ## Plot of clearcutting day and bird survey date
 lims <- c(as.Date("2002-08-01", "%Y-%m-%d"),as.Date("2017-08-01", "%Y-%m-%d"))
@@ -87,6 +113,14 @@ ggplot(forsmarsub, aes(Avvdatum, Date)) +
   labs(x = "Date of clearcutting", y = "Date of RBS sighting") +
   theme_d
 
+## Histogram of age of clear cut at date of observation
+ggplot(forsmarsub, aes(x = clearcutage)) +
+  geom_histogram(binwidth = 1, color = "gray45", fill = "gray80") +
+  labs(x = "Age of clear cut (years)", y = "Number of observations") +
+  scale_x_continuous(expand = c(0,0), limits = c(-1,12)) +
+  scale_y_continuous(expand = c(0,0), limits = c(0,25)) +
+  hist_theme +
+  theme_classic()
 
 ## Histogram of age of clearcuts in the area and age of clearcuts where 
 ## RBS were seen
@@ -118,7 +152,8 @@ ggplot(all, aes(x=Arealha, fill=type)) +
 nestdatelm <- lm(forsmarsub$birdday ~ forsmarsub$clearcutday)
 summary(nestdatelm)
 
-
+observationmodel <- lme(Inds ~ clearcutage + Arealha + 1, random = ~1|Year, data = forsmarsub, method = 'ML')
+summary(observationmodel)
 
 #################
 ## quick stats ##
